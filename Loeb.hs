b4ac551e411ecb007c97a2f512311b93e0b9f814
Eto demerzel <universitas.literarum@gmail.com>
2019-04-03 18:03:36 +0500

    add loebLambda

--- Loeb.hs
+++ Loeb.hs
@@ -1,5 +1,12 @@
 module Loeb where
 
-loeb :: Functor f => f (f a -> a) -> f a
-loeb x = go where go = fmap ($ go) x
+loeb   :: Functor f => f (f a -> a) -> f a
+loeb x = go
+  where
+    go = fmap ($ go) x
 
+
+loebLambda   :: Functor f => f (f a -> a) -> f a
+loebLambda x = go
+  where
+    go = fmap (\y -> y go) x
