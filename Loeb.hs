ccdd796c3c71853d4463061feb334e7d2aa12a81
Eto demerzel <universitas.literarum@gmail.com>
2019-04-05 17:37:10 +0500

    add fix function

--- Loeb.hs
+++ Loeb.hs
@@ -10,3 +10,9 @@ loebLambda   :: Functor f => f (f a -> a) -> f a
 loebLambda x = go
   where
     go = fmap (\y -> y go) x
+
+fs = [ const 1
+     ,succ . (!! 0)
+     ,succ . (!! 1)
+     ,succ . (!! 2)
+     ]
