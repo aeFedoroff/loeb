3fbd42fd7eb5c310e720817e34c0284c039e225a
Eto demerzel <universitas.literarum@gmail.com>
2019-04-08 11:50:41 +0500

    add general recursion function

--- rec.hs
+++ rec.hs
@@ -26,12 +26,21 @@ instance Recursion [a] where
                 [] -> True
                 _  -> False
   prep xl  = case xl of
-               [] -> []
+               []   -> []
                x:xs -> xs
   toInt xl = case xl of
                [] -> 0
                x:xs -> 1 + toInt xs
 
+instance Recursion Integer where
+  ifZero int = case int of
+                 0 -> True
+                 _ -> False
+  prep int   = case int of
+                 0    -> 0
+                 int' -> int' - 1
+  toInt int  = int
+                 
 --generalRecursionFunction :: (Recursion a,Recursion b,Recursion c) => a -> b -> b -> (b -> b) -> b -> b 
 generalRecursionFunction zeroVal coupFunc arg =
   case ifZero(arg) of
@@ -42,7 +51,12 @@ generalRecursionFunction zeroVal coupFunc arg =
 
 --len xl = generalRecursionFunction 0 (+1) xl
 
---len :: [a] -> Int
-len xl zeroVal coupFunc = case ifZero(xl) of
-                            True  -> zeroVal
-                            False -> coupFunc 1 (len (prep xl))
+-- len :: (Recursion b) => [a] -> b -> (b -> b -> b) -> b 
+-- len xl zeroVal coupFunc = case ifZero(xl) of
+--                             True  -> zeroVal
+--                             False -> (+) (((const 1) . head) xl) (len (prep xl))
+
+
+len_mod xl zeroVal = if ifZero(xl)
+                     then zeroVal
+                     else ((+) 1 (len_mod (prep xl) zeroVal))
